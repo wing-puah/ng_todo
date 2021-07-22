f2da125ebe121355c38095ee0cae2dc1ae6e63e4
wing-puah <puahhuiying@gmail.com>
2021-07-22 11:05:42 +0800

    add initial typescript interface

---
+++ src/app/components/note-editor/note-editor.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { HelperBarComponent } from '@components/helper-bar/helper-bar.component';
+
+@Component({
+  selector: 'app-note-editor',
+  templateUrl: './note-editor.component.html',
+  styleUrls: ['./note-editor.component.scss']
+})
+export class NoteEditorComponent implements OnInit {
+  // "app-helper-bar": HelperBarComponent;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
